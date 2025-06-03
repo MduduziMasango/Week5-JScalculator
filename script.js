211b61e44b48aed7fa55ec3bf8a48933cfbb77eb
MduduziMasango <masangomduduzi464@gmail.com>
2025-06-03 10:04:35 -0700

    Lesson 4 - Removing the Zero in the Display

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ let justCalculated = false;
 
 function appendToDisplay(value) {
     console.log('Button pressed:', value);
+    let currentValue = display.value;
+    if (justCalculated && !isNaN(value)) {
+        display.value = value;
+        justCalculated = false;
+        return;
+    }
 
-    alert('You pressed: ' + value);
+        // If current display show 0 and user enters a number, we want to replace the 0
+        if (currentValue === '0' && !isNaN(value)) {
+            display.value = value
+        } else if(currentValue === '0' && value === '.') {
+            display.value = currentValue + value;
+        } else {
+            display.value = currentValue + value;
+        }
+
+
+        // reset the justCalculated flag when user starts typing
+        justCalculated = false;
+        
+        console.log('Display updated to: ', display.value);
 }
 
 function clearDisplay() {
@@ -18,6 +37,14 @@ function clearDisplay() {
 
 function deleteLast() {
     console.log('Backspace button pressed.');
+    let currentValue = display.value;
+
+    // if there's only one charecter or its 0, reset to 0
+    if (currentValue.length <= 1 || currentValue === '0') {
+        display.value = '0';
+    } else {
+        display.value = currentValueslice(0, -1);
+    }
 
     alert('Delete button was pressed')
 }
